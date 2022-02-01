de0212cfdc2a4cc64fda3c7c634b0704d840a90c
Rajan Puri <98840946+purirajan@users.noreply.github.com>
2022-02-01 13:56:30 -0500

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,10 @@
+# factorial
+This python code will print the factorial of the given number. 
+def factorial(n):
+    result = 1
+    for x in range(1,n+1):
+        result = result * x
+    return result
+
+for n in range(1,10):
+    print(n, factorial(n))
