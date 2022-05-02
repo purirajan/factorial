8f8a194e12206b5a4c99033d9d7550c143929391
Rajan Puri <98840946+purirajan@users.noreply.github.com>
2022-05-01 22:56:46 -0400

    Create Fibonacci Sequence.py

---
+++ Fibonacci Sequence.py
@@ -0,0 +1,6 @@
+ def fibonacci_of(n):
+        if n in {0, 1}:  # Base case
+            return n
+        else:
+            return fibonacci_of(n - 1) + fibonacci_of(n - 2)  # Recursive case 
+[fibonacci_of(n) for n in range(15)]
